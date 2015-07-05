4f9166358dd4115b6e4dd9955ab8ff2f1d50ca94
MarcosFPo <MarcosFPo@PRTLDESKTOP>
2015-07-05 15:27:36 -0300

    view_topic finished

--- event/viewtopic_listener.php
+++ event/viewtopic_listener.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- *
- * Group Icon
- *
- * @copyright (c) 2015 MarcosFPo
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* Group Icon
+*
+* @copyright (c) 2015 MarcosFPo
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
 
 namespace marcosfpo\groupicon\event;
 
@@ -27,11 +27,16 @@ class viewtopic_listener implements EventSubscriberInterface
 
     /** @var \phpbb\db\driver\driver_interface */
     protected $db;
+    
+    /** @var \phpbb\template\template */
+    protected $template;
 
-    public function __construct(\phpbb\user $user, \phpbb\db\driver\driver_interface $db)
+
+    public function __construct(\phpbb\user $user, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db)
     {
         $this->user = $user;
         $this->db = $db;
+        $this->template = $template;
     }
 
     /**
@@ -57,34 +62,41 @@ class viewtopic_listener implements EventSubscriberInterface
      * @return null
      * @access public
      */
-    public function viewtopic_modify_post_row($event)
-    {
+     public function viewtopic_modify_post_row($event)
+     {
         $row = $event['row'];
         $user_cache = $event['user_poster_data'];
+        
+        $user_id = $row['user_id'];
 
-        // Groups in viewtopic
-        $sql = 'SELECT group_id, group_name, group_type, group_groupicon_iconpath
-		FROM ' . GROUPS_TABLE . '
-		WHERE group_type <> ' . GROUP_HIDDEN;
+	$sql = 'SELECT g.group_id , g.group_name , g.group_groupicon_iconpath, g.group_type, gu.user_id 
+		FROM ' . GROUPS_TABLE . ' g INNER JOIN ' . USER_GROUP_TABLE . ' gu 
+			ON g.group_id = gu.group_id 
+		WHERE (g.group_groupicon_iconpath <> NULL OR g.group_groupicon_iconpath <>  "") 
+			AND g.group_type <> ' . GROUP_HIDDEN . ' 
+			AND gu.user_id = ' . $user_id;
 
+	$groupsicons = array();
         $result = $this->db->sql_query($sql);
-
         while ($row = $this->db->sql_fetchrow($result))
         {
-            $groups_name[$row['group_id']] = ($row['group_type'] == GROUP_SPECIAL) ? $this->user->lang['G_' . $row['group_name']] : $row['group_name'];
-            $groups_icons[$row['group_id']] = (is $row['group_groupicon_iconpath'] == GROUP_SPECIAL) ? $this->user->lang['G_' . $row['group_name']] : $row['group_name'];
+            $group_name = ($row['group_type'] == GROUP_SPECIAL) ? $this->user->lang['G_' . $row['group_name']] : $row['group_name'];
+            $group_icon = $row['group_groupicon_iconpath'];
+            $groupsicons[$group_name] = $group_icon; 
         }
         $this->db->sql_freeresult($result);
 
-        foreach ($groups_name as $key => $value)
-        {
-            echo("$key  =>  $value <br />");
-        }
-
-
-        $event['post_row'] = array_merge($event['post_row'], array(
-            'POSTER_GROUP' => $groups_name[$user_cache['group_id']],
-        ));
+	$icons = $this->generate_icons($groupsicons);
+	
+	/*
+	$this->template->assign_vars(array(
+		'POSTER_GROUPS_ICONS'	=> $icons,
+	));
+	*/
+	
+	$event['post_row'] = array_merge($event['post_row'], array(
+		'POSTER_GROUPS_ICONS' => $icons,
+	));
     }
 
     /**
@@ -113,6 +125,17 @@ class viewtopic_listener implements EventSubscriberInterface
         $array = $event['user_cache_data'];
         $array['group_id'] = '';
         $event['user_cache_data'] = $array;
+    }
+    
+    protected function generate_icons($groupsicons)
+    {
+    	$return = '';
+    	foreach ($groupsicons as $key=>$val) 
+    	{
+    		$return .= '<span title="' . $key . '" style="padding-right: 3px; padding-top: 3px; display: inline-block;"><img width="48px" height="48px" src="' . $val . '"></span>';
+    	}
+    	
+    	return $return;
     }
 
 }
